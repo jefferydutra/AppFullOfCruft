4197d8e78943986f0bc01c736755b6d42a286162
Jeff Dutra <jeffdutra@outlook.com>
2015-02-23 21:03:53 -0500

    Added a bunch of flux stuff

---
+++ AppFullOfCruft.Web/js/library/src/Components/reactComponents.jsx
@@ -0,0 +1,13 @@
+﻿var React = require("React");
+var CharacterManager = require("./CharacterManager.jsx");
+var SeriesWebAPIUtils = require('../utils/SeriesWebApiUtlis');
+
+SeriesWebAPIUtils.getAllSeries();
+
+window.reactElements = {};
+window.reactElements.loadCharacterManager= function(domId){
+  React.render(
+    <CharacterManager />,
+    document.getElementById(domId)
+  );
+};
